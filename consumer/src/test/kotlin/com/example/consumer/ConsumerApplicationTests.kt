165c22faf378fecaa9b8734be3e24ed238fa3bb5
Eddú Meléndez <eddu.melendez@gmail.com>
2020-04-06 00:04:14 -0500

    Add consumer test

--- consumer/src/test/kotlin/com/example/consumer/ConsumerApplicationTests.kt
+++ consumer/src/test/kotlin/com/example/consumer/ConsumerApplicationTests.kt
@@ -2,12 +2,21 @@ package com.example.consumer
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties
+import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest
+@AutoConfigureStubRunner(ids = ["com.example:producer:+:8080"], stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 class ConsumerApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		WebTestClient.bindToServer()
+		                .build()
+		                .get().uri("http://localhost:8080/languages").exchange()
+		                .expectStatus().isOk
+		                .expectBody().json("""["Java","Groovy","Kotlin"]""")
 	}
 
 }
