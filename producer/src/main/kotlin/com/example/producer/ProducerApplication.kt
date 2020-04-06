5d51a107e8ab7b322da015f194271962a4a3a98d
Eddú Meléndez <eddu.melendez@gmail.com>
2020-04-05 23:55:34 -0500

    Add router bean

--- producer/src/main/kotlin/com/example/producer/ProducerApplication.kt
+++ producer/src/main/kotlin/com/example/producer/ProducerApplication.kt
@@ -2,10 +2,22 @@ package com.example.producer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.json
+import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Flux
 
 @SpringBootApplication
 class ProducerApplication
 
 fun main(args: Array<String>) {
 	runApplication<ProducerApplication>(*args)
+}
+
+@Bean
+fun apiRouter() = router {
+    (accept(MediaType.APPLICATION_JSON) and "/languages").nest {
+        GET("/") { request -> ok().json().body(Flux.just("Java", "Groovy", "Kotlin"), List::class.java) }
+    }
 }
